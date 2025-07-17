5e3f8ac722506d8f3fccf764be661ceb24a64798
BardiaJS <bsardroodi@gmail.com>
2025-07-17 17:09:23 +0330

    debug-code-pt3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -50,16 +50,34 @@
           </form>
 
 
-          @if (Auth::user()->customer)
-          <form action="/shopping-cart" method="GET" class="d-inline">
-            <button class="btn btn-sm" style="background-color: #34699A; color:#FDF5AA; margin-left: 5px;">
-             {{count(Auth::user()->customer->orders)  }}
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-              </svg>               
+          @php
+              $isBanned = \App\Models\Ban::where('user_id', Auth::user()->id)->exists();
+          @endphp
 
-            </button>
-          </form>
+
+
+
+          @if (Auth::user()->customer)
+            @if($isBanned)
+            <form action="/shopping-cart" method="GET" class="d-inline">
+              <button disabled class="btn btn-sm" style="background-color: #34699A; color:#FDF5AA; margin-left: 5px;">
+              {{count(Auth::user()->customer->orders)  }}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                </svg>               
+              </button>
+            </form>
+            @else
+            <form action="/shopping-cart" method="GET" class="d-inline">
+              <button class="btn btn-sm" style="background-color: #34699A; color:#FDF5AA; margin-left: 5px;">
+              {{count(Auth::user()->customer->orders)  }}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                </svg>               
+
+              </button>
+            </form>
+            @endif
           @endif
         </div>
       @else
